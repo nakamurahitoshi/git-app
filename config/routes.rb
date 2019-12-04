20effa762cc8190c06ef47deadf0145b8acbbd9e
nakamurahitoshi <naka.mura.nmhs@gmail.com>
2019-12-04 10:08:08 +0900

    komi

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+namespace :admin do
+  namespace :users do
+    resources :tweets, only [:index]
+  end
+end
 end
